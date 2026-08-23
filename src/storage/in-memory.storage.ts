8c3e00eaf3ce7b9702a2e5be172b6a75a0a95df9
Sauman Arshad <sauman.arshad@gmail.com>
2026-08-23 14:25:33 +0500

    fix: support NestJS v10 and v11 peer dependencies

--- src/storage/in-memory.storage.ts
+++ src/storage/in-memory.storage.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from 'crypto';
-import { CapturedCase, NewCase, ReplayLabStorage } from './storage.interface';
+import { randomUUID } from "crypto";
+import { CapturedCase, NewCase, ReplayLabStorage } from "./storage.interface";
 
 export class InMemoryStorage implements ReplayLabStorage {
   private readonly cases = new Map<string, CapturedCase>();
@@ -7,10 +7,6 @@ export class InMemoryStorage implements ReplayLabStorage {
   async saveCase(newCase: NewCase): Promise<string> {
     const id = randomUUID();
     this.cases.set(id, { ...newCase, id });
-    // TODO: Remove this debug log after e2e testing
-    console.log(
-      `[ReplayLab] Case saved: ${newCase.method} ${newCase.url} (${newCase.responseStatus})`
-    );
     return id;
   }
 
